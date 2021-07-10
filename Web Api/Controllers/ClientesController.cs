f92aef201b675bca92177561d00c9c2c01eafe53
ldinubila <lucas.dinubila@gmail.com>
2021-07-10 01:09:31 -0300

    first commit

---
+++ Web Api/Controllers/ClientesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Api.Modelo;
+
+namespace Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly _20211CTPContext _dbContext;
+
+        public ClientesController(_20211CTPContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
+        {
+            return await _dbContext.Clientes.ToListAsync();
+        }
+
+
+
+    }
+
+
+
+
+}
---
+++ Web Api/Modelo/PedidoArticulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Web_Api.Modelo
+{
+    public partial class PedidoArticulo
+    {
+        public int IdPedido { get; set; }
+        public int IdArticulo { get; set; }
+        public int Cantidad { get; set; }
+
+        public virtual Articulo IdArticuloNavigation { get; set; }
+        public virtual Pedido IdPedidoNavigation { get; set; }
+    }
+}
